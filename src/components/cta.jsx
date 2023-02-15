46b557f9cb84f22e669c03e9414d6a45c24a8c98
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-02-15 21:52:08 +0530

    minor css changes

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -4,7 +4,7 @@ import download from "../assets/download.svg";
 
 function Cta() {
   return (
-    <div id="cta" className="mt-1 flex items-center justify-between">
+    <div id="cta" className="mt-16 flex items-center justify-between">
       <button
         type="button"
         className="inline-flex cursor-pointer items-center justify-center gap-2 rounded-lg border border-textPrimary bg-none px-5 py-4 text-sm font-medium text-textPrimary"
