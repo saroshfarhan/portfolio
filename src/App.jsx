d0dd8809a37e694fc1c86f598899ffbb9e226502
Sarosh Farhan <saroshfarhan6495@gmail.com>
2023-02-15 21:51:13 +0530

    Added CTA section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import IntroHeader from "./components/introHeader";
+import Cta from "./components/cta";
 import data from "../data.json";
 
 function App() {
   return (
     <div className="container mt-10 px-4 lg:mx-auto">
       <IntroHeader introData={data.intro} />
+      <Cta />
     </div>
   );
 }
---
+++ src/components/cta.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import contact from "../assets/contact.svg";
+import download from "../assets/download.svg";
+
+function Cta() {
+  return (
+    <div id="cta" className="mt-1 flex items-center justify-between">
+      <button
+        type="button"
+        className="inline-flex cursor-pointer items-center justify-center gap-2 rounded-lg border border-textPrimary bg-none px-5 py-4 text-sm font-medium text-textPrimary"
+      >
+        Download CV
+        <img src={download} alt="download icon" className="h-[15px] w-[15px]" />
+      </button>
+
+      <button
+        type="button"
+        className="inline-flex cursor-pointer items-center justify-center gap-2 rounded-lg  bg-highlight px-6 py-[18px] text-sm font-medium text-textPrimary hover:bg-[#9154fb]"
+      >
+        Contact Me
+        <img src={contact} alt="contact icon" className="h-[15px] w-[15px]" />
+      </button>
+    </div>
+  );
+}
+
+export default Cta;
